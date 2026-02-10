f8627ba1976fdad086a84120da5785c9024177df
egzonkrs <71973901+egzonkrs@users.noreply.github.com>
2026-02-10 20:33:25 +0100

    feat: add CI/CD pipeline for deployment

--- back-end/src/Icon.Api/Program.cs
+++ back-end/src/Icon.Api/Program.cs
@@ -63,7 +63,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHttpsRedirection();
 }
 
+app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 app.Run();
